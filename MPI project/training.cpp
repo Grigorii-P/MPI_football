2f31fc7ffd193db92635d5028a41cb51a43c7d86
«Grigorii-P» <pogoreloff.g@gmail.com>
2017-11-12 18:05:19 +0800

    everything works except players_positions[][]

--- MPI project/training.cpp
+++ MPI project/training.cpp
@@ -22,13 +22,13 @@ int random_winner(bool *winners, int numPlayers);
 // TODO: прокомментировать всю стратегию
 int main(int argc, char *argv[])  {
     int rank, numtasks;
-    
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     
     int numOfPlayers = numtasks-1;
-    int my_position[2] = {rand() % (ROWS+1), rand() % (COLS+1)};
+    // TODO: rand() не работает, сука
+    int my_position[2] = {rand() % ROWS, rand() % COLS};
     int ball_position[2] = {ROW_BALL_INIT_POS,COL_BALL_INIT_POS};
     // position of the ball after being kicked randomly
     // TODO: можно удалить и сделать ball_position
@@ -38,56 +38,56 @@ int main(int argc, char *argv[])  {
     int winner_index;
 
     for (int i = 0; i < NUM_ROUNDS; i++) {
+        if (rank == 0) printf("ROUND %d, ball position - (%d,%d)\n", i+1, ball_position[0], ball_position[1]);
         MPI_Bcast(&ball_position, 2, MPI_INT, 0, MPI_COMM_WORLD);
-        if (rank == 0) {
-            printf("ROUND %d, ball position - (%d,%d)\n",i+1,ball_position[0],ball_position[1]);
-        }
-        
+
         for (int j = 0; j < NUM_STEPS; j++) {
-             // ---------------- players' part
+
+            // ---------------- field's part
+            if (rank == 0) {
+                for (int p = 1; p <= numOfPlayers; p++) {
+                    MPI_Recv(&(players_positions[p][0]), 2, MPI_INT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                }
+                winner_index = who_is_winner(players_positions, ball_position, numOfPlayers); // if '-1' is returned, no one becomes a winnner
+
+                for (int p = 1; p <= numOfPlayers; p++) {
+                    printf("    player %d, position (%d,%d)\n",p,players_positions[p-1][0],players_positions[p-1][1]);
+                }
+                
+                printf("index of the winner = %d\n",winner_index);
+
+                for (int p = 1; p <= numOfPlayers; p++) {
+                    (p == winner_index) ? winner = true : winner = false;
+                    MPI_Send(&winner, 1, MPI_CXX_BOOL, p, 0, MPI_COMM_WORLD);
+                }
+
+                if (winner_index != -1) {
+                    MPI_Recv(new_ball_position, 2, MPI_INT, winner_index, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    ball_position[0] = new_ball_position[0];
+                    ball_position[1] = new_ball_position[1];
+                    break;
+                }
+            }
+
+            // ---------------- players' part
             if (rank != 0) {
                 printf("rank - %d: ", rank);
                 printf("previous position - (%d,%d), ",my_position[0],my_position[1]);
-                // TODO: возможно, стоит вернуть '&'
                 make_step(my_position, ball_position);
                 printf("current position - (%d,%d)\n",my_position[0],my_position[1]);
-//                MPI_Send(my_position, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
-//                MPI_Recv(&winner, 1, MPI_CXX_BOOL, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//                if (winner) {
-//                    kick_ball(ball_position, new_ball_position);
-//                    // TODO: может быть проблема с адресацией new_ball_position
-//                    MPI_Send(new_ball_position, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
-//                }
+                MPI_Send(my_position, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                MPI_Recv(&winner, 1, MPI_CXX_BOOL, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if (winner) {
+                    kick_ball(ball_position, new_ball_position);
+                    // TODO: может быть проблема с адресацией new_ball_position
+                    MPI_Send(new_ball_position, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                }
             }
-//
-//            // ---------------- field's part
-//            if (rank == 0) {
-//                for (int p = 1; p < numOfPlayers; p++) {
-//                    MPI_Recv(&players_positions[p], 2, MPI_INT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//                }
-//                winner_index = who_is_winner(players_positions, ball_position, numOfPlayers); // if '-1' is returned, no one becomes a winnner
-//
-//                for (int p = 0; p < numOfPlayers; p++) {
-//                    (p == winner_index) ? winner = true : winner = false;
-//                    MPI_Send(&winner, 1, MPI_CXX_BOOL, p, 0, MPI_COMM_WORLD);
-//                }
-//
-//                if (winner_index != -1) {
-//                    MPI_Recv(new_ball_position, 2, MPI_INT, winner_index, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//                    ball_position[0] = new_ball_position[0];
-//                    ball_position[1] = new_ball_position[1];
-//                    break;
-//                }
-//            }
             MPI_Barrier(MPI_COMM_WORLD);
         }
     }
     MPI_Finalize();
 }
-
-
-
-
 
 
 
@@ -150,21 +150,3 @@ int random_winner(bool *winners, int numOfPlayers) {
         if (winners[i] == true) return i+1;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
